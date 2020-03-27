48b4cc3da5a04f0f923d3125d3a4a58ea8e7867b
Peter Rice <price@keplergrp.com>
2020-03-27 14:47:32 -0400

    Updating meter count with max count

--- scenario2/js/scenario2.js
+++ scenario2/js/scenario2.js
@@ -4,12 +4,12 @@ function getCookie(name) {
   if (parts.length == 2) return parts.pop().split(";").shift();
 }
 
-function meterSet(name,duration) {
+function meterSet(name,duration,max) {
   var cookieValue = getCookie(name);
   if (cookieValue == undefined) {
     var cookieValue = 1;
     document.cookie = name + "=" + cookieValue + "; Max-Age=" + duration + "; path=/scenario2; domain=." + location.hostname.replace(/^www\./i, "") + ";secure;samesite=lax";
-  } else {
+  } else if (Number(cookieValue) < max){
     var cookieValue = Number(cookieValue) + 1;
     document.cookie = name + "=" + cookieValue + "; Max-Age=" + duration + "; path=/scenario2; domain=." + location.hostname.replace(/^www\./i, "") + ";secure;samesite=lax";
   }
