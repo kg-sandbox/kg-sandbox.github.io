57ddcca05eba7aa27968422a4745e5a15fa05a81
Peter Rice <price@keplergrp.com>
2020-03-27 11:40:21 -0400

    Fixing meter count script parameter

--- scenario2/js/scenario2.js
+++ scenario2/js/scenario2.js
@@ -1,10 +1,16 @@
+function getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
 function meterSet(name,duration) {
   var meter = getCookie(name);
   if (meter == undefined) {
     var cookieValue = 1;
-    document.cookie = cookieName + "=" + cookieValue + "; Max-Age=" + duration + "; path=/; domain=." + location.hostname.replace(/^www\./i, "") + ";secure;samesite=lax"; // Sets cookie for all subdomains
+    document.cookie = cookieName + "=" + cookieValue + "; Max-Age=" + duration + "; path=/scenario2; domain=." + location.hostname.replace(/^www\./i, "") + ";secure;samesite=lax";
   } else {
     var cookieValue = Number(meter) + 1;
-    document.cookie = cookieName + "=" + cookieValue + "; Max-Age=" + duration + "; path=/; domain=." + location.hostname.replace(/^www\./i, "") + ";secure;samesite=lax"; // Sets cookie for all subdomains
+    document.cookie = cookieName + "=" + cookieValue + "; Max-Age=" + duration + "; path=/scenario2; domain=." + location.hostname.replace(/^www\./i, "") + ";secure;samesite=lax";
   }
 }
