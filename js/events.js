17f90b7068360b8752574b4f94e0f4d35e3a2e24
Peter Rice <price@keplergrp.com>
2020-08-11 17:39:30 -0400

    scen 1 new log

--- js/events.js
+++ js/events.js
@@ -42,6 +42,25 @@ function kgl(i,e,p) {
   fetch(fullUrl)
 }
 
+function dev_kgl(i,e,p) {
+  databaseCookie('_kglid');
+  let scriptId = 'AKfycbznKH4DhQyRy4Uv1Dmcj6N9LkJokfdDQcHqmIIiaNyu4wQDTwTV';
+  let baseUrl = 'https://script.google.com/macros/s/' + scriptId + '/exec?';
+  let logId = 'LogId=' + i;
+  let userId = 'UserId=' + getCookie('_kglid');
+  //let userId = 'UserId=' + 'uid';
+  let hostname = 'Domain=' + window.location.hostname;
+  //let hostname = 'Domain=' + 'hname';
+  let path = 'Path=' + window.location.pathname;
+  //let path = 'Path=' + 'pname';
+  let referrer = 'Referrer=' + document.referrer;
+  //let referrer = 'Referrer=' + 'rfer';
+  let action = 'Action=' + e;
+  var values = [logId,userId,hostname,path,referrer,action,serialize(p)];
+  var fullUrl = baseUrl + values.filter(Boolean).join('&');
+  fetch(fullUrl)
+}
+
 /*
 kgLog('123','PageView1',{
   key1: "value1",
